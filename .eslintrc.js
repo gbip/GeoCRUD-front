5098d3a3a8c7dd4bfb6caddb93b0287f9fe2b674
J-E Castagnede <j.e.castagnede@gmail.com>
2021-07-22 12:55:42 +0200

    fix eslint invalid rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,6 @@ module.exports = {
     'react/no-this-in-sfc': ['error'],
     'react/no-access-state-in-setstate': ['error'],
     'import/no-cycle': ['off'],
-    'space-before-function-paren': ['never'],
+    'space-before-function-paren': ['off'],
   },
 };
