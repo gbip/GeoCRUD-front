51590ba0dca57d7343576c94fb06e4f2c16c7e1b
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-08-27 17:46:11 +0200

    Fix type.test.js

--- src/modules/CRUD/components/Details/PropertyItem/Type/Type.test.js
+++ src/modules/CRUD/components/Details/PropertyItem/Type/Type.test.js
@@ -11,12 +11,12 @@ import ObjectType from './ObjectType';
 import NumberType from './NumberType';
 import StringType from './StringType';
 
-jest.mock('./ArrayType', () => jest.fn().mockName('ArrayType'));
-jest.mock('./BooleanType', () => jest.fn().mockName('BooleanType'));
-jest.mock('./FileType', () => jest.fn().mockName('FileType'));
-jest.mock('./ObjectType', () => jest.fn().mockName('ObjectType'));
-jest.mock('./NumberType', () => jest.fn().mockName('NumberType'));
-jest.mock('./StringType', () => jest.fn().mockName('StringType'));
+jest.mock('./ArrayType', () => 'ArrayType');
+jest.mock('./BooleanType', () => 'BooleanType');
+jest.mock('./FileType', () => 'FileType');
+jest.mock('./ObjectType', () => 'ObjectType');
+jest.mock('./NumberType', () => 'NumberType');
+jest.mock('./StringType', () => 'StringType');
 
 jest.spyOn(global.console, 'warn');
 
@@ -25,7 +25,7 @@ beforeEach(() => {
 });
 
 it('should get component', () => {
-  expect(getComponent('string').getMockName()).toEqual('StringType');
+  expect(getComponent('string')).toEqual('StringType');
 });
 
 it('should not get component', () => {
@@ -45,99 +45,99 @@ describe('should get the right type', () => {
 
 describe('should call the appropriate component', () => {
   it('Array', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={['array', 'value']}
         schema={{
           type: 'array',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<ArrayType />)).toBe(true);
   });
   it('Boolean', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={false}
         schema={{
           type: 'boolean',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<BooleanType />)).toBe(true);
   });
   it('File component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={{ url: 'http://www.foo.fr/pdf.pdf' }}
         schema={{
           type: 'string',
         }}
         type="file"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<FileType />)).toBe(true);
   });
   it('Image component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={{ url: 'http://www.foo.fr/img.jpg' }}
         schema={{
           type: 'string',
         }}
         type="image"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<FileType />)).toBe(true);
   });
   it('Number component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={42.5}
         schema={{
           type: 'number',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<NumberType />)).toBe(true);
   });
   it('Integer component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={42}
         schema={{
           type: 'number',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<NumberType />)).toBe(true);
   });
   it('Object component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value={{ foo: 'foo', bar: 'bar' }}
         schema={{
           type: 'object',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<ObjectType />)).toBe(true);
   });
   it('String component', () => {
-    const wrapper = shallow((
+    const wrapper = shallow(
       <Type
         display_value="String value"
         schema={{
           type: 'string',
         }}
         type="string"
-      />
-    ));
+      />,
+    );
     expect(wrapper.matchesElement(<StringType />)).toBe(true);
   });
 });
